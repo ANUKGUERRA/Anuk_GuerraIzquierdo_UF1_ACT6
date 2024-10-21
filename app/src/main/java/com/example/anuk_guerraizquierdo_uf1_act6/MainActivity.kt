d66d66336ee9fd06bf3d30cdbc2906cf2673a18b
ANUKGUERRA <anukguerra@gmail.com>
2024-10-21 13:02:54 +0200

    Finalitzat

---
+++ app/src/main/java/com/example/anuk_guerraizquierdo_uf1_act6/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.example.anuk_guerraizquierdo_uf1_act6
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        val submitButton = findViewById<Button>(R.id.submit)
+        val inputText = findViewById<TextView>(R.id.inputText)
+
+        submitButton.setOnClickListener{
+            Toast.makeText(this, inputText.text.toString(), Toast.LENGTH_SHORT).show()
+        }
+    }
+}
